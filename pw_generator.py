a604b5999abb1e4962f1a1ad0ea38d95f7d3f9f3
Jessica Amos <amosjessicar@gmail.com>
2021-11-19 16:15:28 -0500

    Create pw_generator.py

---
+++ pw_generator.py
@@ -0,0 +1,8 @@
+# LOOPS (:
+# this loop prints out each item in the list fruits
+# it uses fruit as a variable and switches upon each iteration
+# fruits = ["apple", "peach", "pear"]
+# for fruit in fruits:
+    #print(fruit)
+# loops allow us to execute statements several times
+
