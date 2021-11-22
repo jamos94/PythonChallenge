56c8601ab257c903fcea5cdbc64701866f7020f5
Jessica Amos <amosjessicar@gmail.com>
2021-11-22 14:59:59 -0500

    hangman complete

--- hangman/hang_man.py
+++ hangman/hang_man.py
@@ -14,101 +14,50 @@
         #GAME OVER
 
 import random
+from hangman_art import logo, stages
+from hangman_words import word_list
+import os
 
-stages = ['''
-  +---+
-  |   |
-  O   |
- /|\  |
- / \  |
-      |
-=========
-''', '''
-  +---+
-  |   |
-  O   |
- /|\  |
- /    |
-      |
-=========
-''', '''
-  +---+
-  |   |
-  O   |
- /|\  |
-      |
-      |
-=========
-''', '''
-  +---+
-  |   |
-  O   |
- /|   |
-      |
-      |
-=========''', '''
-  +---+
-  |   |
-  O   |
-  |   |
-      |
-      |
-=========
-''', '''
-  +---+
-  |   |
-  O   |
-      |
-      |
-      |
-=========
-''', '''
-  +---+
-  |   |
-      |
-      |
-      |
-      |
-=========
-''']
-
-word_list = ["aardvark", "baboon", "camel"]
-chosen_word = ""
-empty_letter = "_"
-end_game = False
-word_length = len(chosen_word)
 chosen_word = random.choice(word_list)
+word_length = len(chosen_word)
+end_game = False
 lives = 6
 # Testing code
+
+print(logo)
 print(f'Pssst, the solution is {chosen_word}.')
-
-
 # For each letter in the chosen_word, add a "_" to 'display'.
 # So if the chosen_word was "apple", display should be ["_", "_", "_", "_", "_"] with 5 "_" representing each letter to guess.
 display = []
 
-for letter in chosen_word:
+for _ in chosen_word:
     display.append("_")  # for each character in the chosen word a "_" is added using append()
-
 
 # loop continues until all blanks are filled 
 while not end_game:
     guess = input("Guess a letter: ").lower()
+
+    if guess in display:
+        print(f"You already guessed '{guess}'")
+
     # checking is guess in in chosen word
     for position in range(word_length):
         letter = chosen_word[position]
         if letter == guess:
-            letter = display[position]
+            display[position] = letter
+
+    # guess is not a letter in the word
     if guess not in chosen_word:
         lives -= 1
+        print(f"Your guess '{guess}'  is not a letter in the word. You lose a life." )
         if lives == 0:
             end_game = True
             print("you lose")
 
-
     #Join all the elements in the list and turn it into a String.
     print(f"{' '.join(display)}")
 
+   # if there are no more blanks, you've won!
     if '_' not in display:
         end_game = True
         print("You won!")
