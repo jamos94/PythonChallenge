30739b2ceeba43b46bbfabec30ae8a319041ee9c
Jessica Amos <amosjessicar@gmail.com>
2021-11-26 22:04:17 -0500

    Update blind_auction.py

--- blind auction/blind_auction.py
+++ blind auction/blind_auction.py
@@ -2,21 +2,35 @@
 from art import logo
 
 
-def new_bid(name, bid):
-    blind_bids[name] = bid
 
 print(logo)
 
+# name = input("What's your name?:\n ")
+# bid = input("What's your bid?: \n$")
+bids = {}
+name = ""
+bid = 0
+
+
+def new_bid(name, bid):
+    new_bidder = True
+    while new_bidder ==True:
+        name = input("What's your name?:\n ")
+        bid = input("What's your bid?: \n$")
+        bids[name] = bid
+        new_bidder = input("Is there another bidder?")
+        if new_bidder == "y":
+            new_bid(name, bid)
+        else:
+            new_bidder == False
 # TODO: create a dictionary 
-    # name : bid
-another_bid = True
-while another_bid == True:
-    blind_bids = {}
-    name = input("what's your name?")
-    bid = input("what's your bid?")
-    new_bid(name, bid)
-    new_bidder = input("is there another bidder? Y/N: ").lower()
-    if new_bidder == "n":
-        another_bid = False
+# name : bid
+new_bid(name, bid)
+
 #TODO: made the highest bid?
-print(blind_bids)
+for bid in bids:
+    if bid > bid:
+        bid[0] = bid
+    print(bid)
+
+print(bids)
