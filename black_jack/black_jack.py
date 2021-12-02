d4fca65e098049dfb73b5717fb2690e0321a306c
Jessica Amos <amosjessicar@gmail.com>
2021-12-02 13:01:27 -0500

    create functions
    calculate scores
    deal cards
    error in loop, cannot update element where ace is located

--- black_jack/black_jack.py
+++ black_jack/black_jack.py
@@ -8,9 +8,9 @@ def deal_card():
 blackjack = 21
 
 # deal out 2 starting cards for player and dealer
-player_hand = []
+player_hand = [11]
 dealer_hand = []
-player_hand.append(deal_card())
+#player_hand.append(deal_card())
 player_hand.append(deal_card())
 
 dealer_hand.append(deal_card())
@@ -19,3 +19,22 @@ dealer_hand.append(deal_card())
 print(f"You were delt {player_hand}.")
 print(f"The dealer was delt {dealer_hand}.")
 
+scores = [
+    sum(dealer_hand),
+    sum(player_hand)
+        ] 
+
+print(f"Player score: {scores[1]}\n Dealer score: {scores[0]}")
+
+
+if scores[0] != 21:
+    if scores[1] ==21:
+        print("BlackJack, player wins")
+    else: #scores[1] > 21:
+        for card in player_hand:
+            if (11 in player_hand):
+                print("you have an ace")
+                print(player_hand)
+            
+else:
+    print("BlackJack, dealer wins")
